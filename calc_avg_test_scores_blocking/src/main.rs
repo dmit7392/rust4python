e4e2060bc2143caf8130ed918cb768b3c44eb2f1
Dmitry Andreev <dandreev@esri.com>
2025-05-08 15:33:27 -0700

    8 -> 9

--- calc_avg_test_scores_blocking/src/main.rs
+++ calc_avg_test_scores_blocking/src/main.rs
@@ -1,10 +1,13 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::PathBuf;
+use std::sync::{Arc, Mutex};
+use std::thread::{self, JoinHandle};
 
 use anyhow::Result;
 
-fn process_file(path: &PathBuf, num: usize) -> io::Result<()> {
+fn process_file(path: &PathBuf) -> Result<f64> {
     //let path = "../../data/biology_test1_scores.csv";
 
     let file = File::open(path)?;
@@ -28,23 +31,52 @@ fn process_file(path: &PathBuf, num: usize) -> io::Result<()> {
         }
     }
 
-    if count > 0 {
-        let average = total_score as f64 / count as f64;
-        println!("Test #{num}: {:.2}", average);
-    } else {
-        println!("No scores found in the file.");
-    }
-
-    Ok(())
+    Ok(
+        if count > 0 {
+            let average = total_score as f64 / count as f64;
+            average
+        }
+        else {
+            std::f64::NAN
+        }
+    )
 }
 
 fn main() -> Result<()> {
+
+    let map: HashMap<PathBuf, f64> = HashMap::new();
+    let data = Arc::new(Mutex::new(map));
+
     let file_paths = std::fs::read_dir("../data")?;
+    let mut handles = vec![];
+
 
     for (i, entry) in file_paths.enumerate() {
-        let entry = entry?;
-        let path = entry.path();
-        process_file(&path, i)?;
+        let path = entry?.path();
+        let data = Arc::clone(&data);
+        let handle = thread::spawn(move || {
+            let val_or_err = process_file(&path);
+
+
+            match val_or_err {
+                Ok(val) => {
+                    println!("{path:?} => {val}");
+                },
+                Err(e) => {
+                    println!("{path:?} => no way");                    
+                }
+            }
+
+        });
+
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        match handle.join() {
+            Ok(_) => {},
+            Err(_) => {},
+        }
     }
 
     Ok(())
